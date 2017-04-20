07acb6cb238fbfc86f2151e44fdbb15cf26d3482
ArttuU <arthur.newmansion@gmail.com>
2017-04-20 11:12:22 +0300

    Ehk vähän parempi ase

---
+++ UnityProjektiEEAU/Assets/_Scripts/MoveBullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBullet : MonoBehaviour {
+
+	public float speed = 1f;
+
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+		transform.Translate (0, 0, speed);
+	}
+}
---
+++ UnityProjektiEEAU/Assets/_Scripts/Shoot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bullet;
+	public float delayTime = 8;
+
+	public AudioClip shootSound;
+	private AudioSource myAudioSource;
+
+	private float counter = 0;
+
+
+	void Awake () 
+	{
+		myAudioSource = GetComponent<AudioSource>();
+
+	}
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.Mouse0) && counter > delayTime) 
+		{
+			Instantiate (bullet, transform.position, transform.rotation);
+			counter = 0;
+		}
+		counter += Time.deltaTime;
+	}
+}
