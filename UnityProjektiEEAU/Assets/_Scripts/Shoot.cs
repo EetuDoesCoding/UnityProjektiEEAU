37eb465005794b60334304e82e68e7f843f790c9
ArttuU <arthur.newmansion@gmail.com>
2017-04-24 12:18:04 +0300

    Thicc

--- UnityProjektiEEAU/Assets/_Scripts/Shoot.cs
+++ UnityProjektiEEAU/Assets/_Scripts/Shoot.cs
@@ -5,41 +5,52 @@ using UnityEngine;
 public class Shoot : MonoBehaviour {
 
 	public GameObject bullet;
-	public float delayTime = 8;
 
-	public AudioClip shootSound;
-	private AudioSource myAudioSource;
+	public int damagePerShot = 20; 
+	public float delayTime = 8;
+	public float range = 100f;
+
+	AudioSource gunAudio;
+
+
 
 	private float counter = 0;
 
 
 	void Awake () 
 	{
-
+		gunAudio = GetComponent<AudioSource> ();
 	}
 	// Use this for initialization
 	void Start () 
 	{
 		
 	}
-	
-	// Update is called once per frame
+
 	void Update () 
 	{
-		myAudioSource = GetComponent<AudioSource>();
+		counter += Time.deltaTime;
 
-		if (Input.GetKey (KeyCode.Mouse0) && counter > delayTime) 
+
+		// If the Fire1 button is being press and it's time to fire...
+		if(Input.GetButton ("Fire1") && counter >= delayTime)
 		{
 			Instantiate (bullet, transform.position, transform.rotation);
 			counter = 0;
+
+			gunAudio.Play ();
+		}
+
+//		if (Input.GetKey (KeyCode.Mouse0) && counter > delayTime) 
+//		{
+//			Instantiate (bullet, transform.position, transform.rotation);
+//			counter = 0;
 
 //			RaycastHit hit;
 //			Ray ray = new Ray (transform.position, transform.forward);
 //			if (Physics.Raycast (ray, out hit, 100f)) 
 //			{
-//				Instantiate ();
+//				Instantiate  ();
 //			}
 		}
-		counter += Time.deltaTime;
-	}
 }
