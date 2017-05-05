08d42dd19b9dab25d7c699f184a1e4957c6def03
ArttuU <arthur.newmansion@gmail.com>
2017-05-05 14:44:59 +0300

    Et lähe

---
+++ UnityProjektiEEAU/Assets/_Scripts/WeaponSwitch.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour {
+
+	[SerializeField]
+	public GameObject weapon01;
+	[SerializeField]
+	public GameObject weapon02;
+
+	public bool showItem1;
+	public bool showItem2;
+
+	// Use this for initialization
+	void Start () 
+	{
+		showItem1 = true;
+		showItem2 = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (showItem1 == false) 
+		{
+			weapon01.SetActive (false);
+		}
+		if (showItem1 == true) 
+		{
+			weapon01.SetActive (true);
+		}
+		if (showItem2 == false) 
+		{
+			weapon02.SetActive (false);
+		}
+		if (showItem2 == true) 
+		{
+			weapon02.SetActive (true);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha1) && showItem1 == false) 
+		{
+			showItem1 = true;
+			showItem2 = false;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2) && showItem2 == false) 
+		{
+			showItem1 = false;
+			showItem2 = true;
+		}
+	}
+}
