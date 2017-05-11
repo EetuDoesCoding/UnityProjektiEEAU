67ff97e9408a80ecaf68699baf9cb1aacd693bf6
ArttuU <arthur.newmansion@gmail.com>
2017-05-11 10:04:51 +0300

    Revolveri jee

--- UnityProjektiEEAU/Assets/_Scripts/WeaponSwitch.cs
+++ UnityProjektiEEAU/Assets/_Scripts/WeaponSwitch.cs
@@ -10,10 +10,14 @@ public class WeaponSwitch : MonoBehaviour {
 	public GameObject weapon02;
 	[SerializeField]
 	public GameObject weapon03;
+	[SerializeField]
+	public GameObject weapon04;
 
 	public bool showItem1;
 	public bool showItem2;
 	public bool showItem3;
+	public bool showItem4;
+
 
 	// Use this for initialization
 	void Start () 
@@ -21,8 +25,10 @@ public class WeaponSwitch : MonoBehaviour {
 		showItem1 = true;
 		showItem2 = false;
 		showItem3 = false;
+		showItem4 = false;
 	}
 	
+
 	// Update is called once per frame
 		
 	void Update () 
@@ -51,24 +57,44 @@ public class WeaponSwitch : MonoBehaviour {
 		{
 			weapon03.SetActive (true);
 		}
+		if (showItem4 == false) 
+		{
+			weapon04.SetActive (false);
+		}
+		if (showItem4 == true) 
+		{
+			weapon04.SetActive (true);
+		}
+
+
 
 		if (Input.GetKeyDown (KeyCode.Alpha1) && showItem1 == false) 
 		{
 			showItem1 = true;
 			showItem2 = false;
 			showItem3 = false;
+			showItem4 = false;
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha2) && showItem2 == false) 
 		{
 			showItem1 = false;
 			showItem2 = true;
 			showItem3 = false;
+			showItem4 = false;
 		}
 		if (Input.GetKeyDown (KeyCode.Alpha3) && showItem3 == false) 
 		{
 			showItem1 = false;
 			showItem2 = false;
 			showItem3 = true;
+			showItem4 = false;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha4) && showItem4 == false) 
+		{
+			showItem1 = false;
+			showItem2 = false;
+			showItem3 = false;
+			showItem4 = true;
 		}
 	}
 }
