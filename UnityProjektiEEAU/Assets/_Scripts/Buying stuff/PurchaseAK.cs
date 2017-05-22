1618794433e14b40d99163cb227ca8dbf5f739f9
ArttuU <arthur.newmansion@gmail.com>
2017-05-22 11:37:57 +0300

    EI TAAS TOIMI, JEE

---
+++ UnityProjektiEEAU/Assets/_Scripts/Buying stuff/PurchaseAK.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PurchaseAK : MonoBehaviour {
+
+
+public Text priceText;
+public int PriceValue = 50;
+
+
+public bool current = false;
+//Declaring variables
+bool crossedBoundary;
+
+
+	void Awake ()
+	{
+//		gameObject.SetActive (true);
+//		current = false;
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+			crossedBoundary = true;
+	}
+
+
+
+	void OnGUI () 
+	{
+		//If the boolean is active, display the text
+		if (crossedBoundary == true) 
+		{
+			priceText.text = "Purchase Weapon (cost: 500)";
+			if (Input.GetKeyDown("f") && ScoreManager.score >= PriceValue)
+			{
+				Buy ();
+			}
+		}
+
+		if (crossedBoundary == false) 
+		{
+			priceText.text = "";
+		}
+	}
+
+
+	void Buy ()
+	{
+		ScoreManager.score -= PriceValue;
+		current = true;		
+		priceText.text = "";
+//		Destroy(this.gameObject);
+	}
+
+
+	void OnTriggerExit(Collider other) 
+	{
+		current = false;
+		crossedBoundary = false;
+	}
+
+}
---
+++ UnityProjektiEEAU/Assets/_Scripts/Buying stuff/PurchaseDoor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PurchaseDoor : MonoBehaviour {
+
+public int PriceValue = 50;
+public Text priceText;
+//Declaring variables
+bool crossedBoundary;
+
+
+	void Start ()
+	{
+		gameObject.SetActive (true);
+
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+			crossedBoundary = true;
+	}
+
+
+
+	void OnGUI () 
+	{
+		//If the boolean is active, display the text
+		if (crossedBoundary == true) 
+		{
+			priceText.text = "Open Barricade (cost: 500)";
+			if (Input.GetKeyDown("f") && ScoreManager.score >= PriceValue)
+			{
+				Buy ();
+			}
+		}
+
+		if (crossedBoundary == false) 
+		{
+			priceText.text = "";
+		}
+	}
+
+
+	void Buy ()
+	{
+		ScoreManager.score -= PriceValue;
+		gameObject.SetActive (false);
+		priceText.text = "";
+//		Destroy(this.gameObject);
+	}
+
+
+	void OnTriggerExit(Collider other) 
+	{
+		crossedBoundary = false;
+	}
+
+}
